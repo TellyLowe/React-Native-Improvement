e9ff09e5e4d21c975be7289aa8446c4906fee469
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-10-13 15:57:54 -0400

    Buttons and Navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Whats Poppin</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Whats Poppin</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to Componentss Demo"
+      />
+      <Button 
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
