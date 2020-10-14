6caf72676e61650fb2c36d231610152c6f137e6d
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-10-13 21:37:26 -0400

    Image Details

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { Text, StyleSheet, View } from "react-native";
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>
+    return (
+        <View>
+            <ImageDetail
+                title="Forest"
+                imageSource={require('../../assets/forest.jpg')}
+                score={9}
+            />
+            <ImageDetail
+                title="Beach"
+                imageSource={require('../../assets/beach.jpg')}
+                score={7}
+            />
+            <ImageDetail
+                title="Mountain"
+                imageSource={require('../../assets/mountain.jpg')}
+                score={10}
+            />
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
